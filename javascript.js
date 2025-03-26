8c211096f7aa42505d212488992cdcbcd60e635c
Gargi_Gunapala <gargigunapala28@gmail.com>
2025-03-26 18:56:13 +0530

    initial project file

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function backtoTop() {
+    const topsection = document.getElementById("top");
+    topsection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToContact() {
+    const contactSection = document.getElementById("contacts");
+    contactSection.scrollIntoView({ behavior: "smooth" });
+}
